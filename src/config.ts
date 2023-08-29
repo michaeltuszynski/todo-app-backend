c97bef19f87e445cd9e10f27838b8d3deac5147e
Michael Tuszynski <miketuszynski42@gmail.com>
2023-08-29 13:55:24 -0700

    removed env production for real env variables

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,14 @@ import * as dotenv from "dotenv";
 
 let path;
 switch (process.env.NODE_ENV) {
-    case "production":
-        path = `${__dirname}/../.env.production`
-        console.log(path);
+    case "development" || "test":
+        path = `${__dirname}/../.env.development`
+        dotenv.config({ path: path })
         break;
     default:
-        path = `${__dirname}/../.env.development`
+        path = "";
+        break;
 }
-dotenv.config({ path: path })
 
 export default {
     MONGODB_URI: process.env.MONGODB_URI,

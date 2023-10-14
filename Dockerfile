e3ed4a33645385cd4af0b9c62a035f858d58332c
Michael Tuszynski <miketuszynski42@gmail.com>
2023-10-14 16:46:31 -0700

    try4

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 #FROM public.ecr.aws/docker/library/node:18.17.1-bullseye AS builder
-FROM node:16.16.0-alpine AS builder
+FROM node:14-slim AS builder
 WORKDIR /app
 COPY . .
 RUN yarn install
 RUN yarn build
 
 #FROM public.ecr.aws/docker/library/node:18.17.1-bullseye AS final
-FROM node:16.16.0-alpine AS final
+FROM node:14-slim AS final
 WORKDIR /app
 COPY --from=builder ./app/dist ./dist
 COPY package.json .
--- src/server.ts
+++ src/server.ts
@@ -1,134 +1,103 @@
-// server.ts
-
 import express from 'express';
-import cors from 'cors';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import { initializeApp, credential, firestore } from 'firebase-admin';
 import bodyParser from 'body-parser';
-import connectToDatabase from './db';
 import config from './config';
 
 const app = express();
 app.use(bodyParser.json());
-let Todo = connectToDatabase();
-const PORT = config.NODEPORT;
+const port = config.NODEPORT || 5000;
 
-// if (process.env.NODE_ENV !== "test") {
-//     Todo = connectToDatabase();
-// }
+const getFirebaseCredentialsFromSecretManager = async () => {
+  const client = new SecretManagerServiceClient();
+  const secretName = `projects/${config.PROJECT_ID}/secrets/${config.SECRET_NAME}/versions/latest`;
+  const [version] = await client.accessSecretVersion({name: secretName});
+  const payload = version.payload?.data?.toString() || '';
+  return JSON.parse(payload);
+};
 
 app.get('/health', async (req, res) => {
-    const healthcheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
     try {
-        res.send(healthcheck);
-    } catch (error:any) {
-        healthcheck.message = error;
-        res.status(503).send();
+      const firebaseCredentials = await getFirebaseCredentialsFromSecretManager();
+      initializeApp({
+        credential: credential.cert(firebaseCredentials),
+      });
+
+      res.status(200).send('API is working');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
     }
-});
+  });
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept'],
-  }));
+  const collectionName = 'myData'; // Specify your collection name
 
-app.use('/todos', cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept']
-  }));
-
-//List all todos
-
-// Get All ToDos
-app.get('/todos', cors(), async (_req, res) => {
+  app.post('/todos', async (req, res) => {
     try {
-        const todosRef = Todo.collection('todos');
-        const snapshot = await todosRef.get();
-        const todos = snapshot.docs.map((doc:any) => ({id: doc.id, ...doc.data()}));
-
-        res.json(todos);
-    } catch (error:any) {
-        res.status(500).send(error.toString());
+      const { data } = req.body;
+      const db = firestore();
+      const docRef = await db.collection(collectionName).add(data);
+      res.status(201).send({ id: docRef.id });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
     }
-});
+  });
 
-
-//List a single todo
-app.get('/todos/:id', cors(), async (req, res) => {
+  app.get('/todos', async (req, res) => {
     try {
-        const {id} = req.params;
-        const todoRef = Todo.collection('todos').doc(id);
-        const doc = await todoRef.get();
+      const db = firestore();
+      const snapshot = await db.collection(collectionName).get();
+      const allData: any[] = [];
+      snapshot.forEach((doc) => allData.push({ id: doc.id, ...doc.data() }));
+      res.status(200).send(allData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
-        if (!doc.exists) {
-          res.json('ToDo not found');
-        } else {
-          res.json({id: doc.id, ...doc.data()});
-        }
-      } catch (error:any) {
-        res.status(500).send(error.toString());
-      }
-});
-
-//Create One Todo
-app.post('/todos', cors(), async (req, res) => {
+  app.get('/todos/:id', async (req, res) => {
     try {
-        const {title} = req.body;
-        const todosRef = Todo.collection('todos');
-        const docRef = await todosRef.add({title, completed: false});
-        res.json({id: docRef.id});
-      } catch (error:any) {
-        res.json(error.toString());
+      const { id } = req.params;
+      const db = firestore();
+      const doc = await db.collection(collectionName).doc(id).get();
+      if (doc.exists) {
+        res.status(200).send({ id: doc.id, ...doc.data() });
+      } else {
+        res.status(404).send('Not Found');
       }
-});
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
-// Update a todo
-app.put('/todos/:id', cors(), async (req, res) => {
+  app.put('/todos/:id', async (req, res) => {
     try {
-        const {id} = req.params;
-        const {title, completed} = req.body;
-        const todoRef = Todo.collection('todos').doc(id);
+      const { id } = req.params;
+      const { data } = req.body;
+      const db = firestore();
+      await db.collection(collectionName).doc(id).set(data, { merge: true });
+      res.status(200).send('Updated Successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
-        await todoRef.update({title, completed});
-
-        res.json({id, title, completed});
-      } catch (error:any) {
-        res.status(500).send(error.toString());
-      }
-});
-
-// Delete a todo
-app.delete('/todos/:id', cors(), async (req, res) => {
+  app.delete('/todos/:id', async (req, res) => {
     try {
-        const {id} = req.params;
-        const todoRef = Todo.collection('todos').doc(id);
+      const { id } = req.params;
+      const db = firestore();
+      await db.collection(collectionName).doc(id).delete();
+      res.status(200).send('Deleted Successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
-        await todoRef.delete();
-
-        res.json();
-      } catch (error:any) {
-        res.json(error.toString());
-      }
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-export default app;
-
-// Start the server only if this script is the main module
-if (require.main === module) {
-    app.listen(PORT!, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
-
-
-
-
-
-
-
-
-
